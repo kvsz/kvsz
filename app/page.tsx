43e932fcb8b821a880a08ac3324fbbe53fb47db5
kvsz <soryehasagi69@gmail.com>
2026-06-11 04:50:19 -0300

    followings +1

--- app/page.tsx
+++ app/page.tsx
@@ -112,7 +112,7 @@ function InstagramModal({ open, onClose }: { open: boolean; onClose: () => void 
     avatar: 'https://i.pinimg.com/736x/88/e5/30/88e53008571209403287e72da264710e.jpg',
     posts: 0,
     followers: 23,
-    following: 30
+    following: 31
   }
 
   useEffect(() => {
